98a4ea051c687eba3905ad7cd061dffcad0931fd
Atharva Ranade <atharvaranade4@gmail.com>
2022-11-01 19:04:04 -0700

    start of class

--- battleGame.js
+++ battleGame.js
@@ -1,11 +1,32 @@
 // 1. Create attack function below.  This will take the following parameters:
 // attackingPlayer, defendingPlayer, baseDamage, variableDamage
+const attack = 
+    (attackingPlayer, 
+    defendingPlayer, 
+    baseDamage, 
+    variableDamage
+    ) => {
+    // generate random number btw baseDamage and variableDamage
+    let RandomDamage = Math.floor(Math.random() * variableDamage + 1);
+    let totalDamage = baseDamage + RandomDamage;
+    defendingPlayer.health -= totalDamage;
+    return `${attackingPlayer.name} hits ${defendingPlayer.name} for ${totalDamage} damage`
+}
+
+
 
 
 
 // 2. Create player1 and player2 objects below
 // Each should have a name property of your choosing, and health property equal to 10
-
+const player1 = {
+  name:"Kevin",
+  health: 10,
+}
+const player2 = {
+  name:"Brian",
+  health: 10,
+}
 
 
 // 3. Refactor attack function to an arrow function.  Comment out function above.
